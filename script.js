5bc8fbfb527d78c3008c9894f440eda3e199265a
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2020-12-04 13:55:40 -0800

    js: declared constants

---
+++ script.js
@@ -0,0 +1,6 @@
+const numbers = document.querySelectorAll('.number');
+const lines = document.querySelectorAll('.lines');
+const buttonsContainer = document.querySelector('.buttons');
+console.log(buttonsContainer);
+console.log(numbers);
+
