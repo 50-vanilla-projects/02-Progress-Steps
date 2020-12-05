808968ff3fe4bc972081fb5196771063a1cf055d
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2020-12-04 17:24:46 -0800

    js: added next button functionality

--- script.js
+++ script.js
@@ -8,8 +8,35 @@ var count = 0;
 
 buttonsContainer.addEventListener('click', e => {
 
+  /* next button clicked */
+  if (e.target.id == 'next') {
+    console.log('next button clicked');
+    console.log('current count: ' + count);
 
+    if (count > 0 && count < 3) {
+      console.log('this number active, increase count');
+      numbers[count].classList.add('active');
+      ++count;
+    }
+    else if (count == 0) {
+      console.log('previous button toggled, this number active, increase count');
+      togglePreviousButton();
+      numbers[count].classList.add('active');
+      ++count;
+    }
+    else if (count == 3) {
+      console.log('this number active, increase count');
+      numbers[count].classList.add('active');
+      ++count;
 
+      console.log('next button toggled')
+      toggleNextButton();
+
+    } else if (count == 4) {
+      console.log('action disabled')
+    }
+    console.log('current count: ' + count)
+  }
 
 function toggleNextButton() {
   if (nextButton.classList.contains('disabled')) {
